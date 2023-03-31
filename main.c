bbeb635b4efd05c7f88071eed960aacb2e341777
Mustafa Mohammed <mustafamohammed0321@gmail.com>
2023-04-01 00:40:31 +0200

    last change

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #define _CRT_NONSTDC_NO_WARNINGS
 
-
+#include<stdio.h>
 
 #include"Terminal/terminal.h"
 #include"Card/card.h"
@@ -19,7 +19,43 @@ int main(void)
 
 	ST_terminalData_t t1;
 
-	EN_terminalError_t Datestate = TERMINAL_OK , Amountstate = TERMINAL_OK , Maxamountstate = TERMINAL_OK ;
+	ST_cardData_t c1; 
+
+	EN_terminalError_t Datestate = TERMINAL_OK , Amountstate = TERMINAL_OK , Maxamountstate = TERMINAL_OK , maxtransamountstate = TERMINAL_OK , cardexpirestatus = TERMINAL_OK;
+	
+	EN_cardError_t cardstatus = CARD_OK;
+	
+	do /* must ask for transaction date first */
+	{
+		Datestate = getTransactionDate(&t1); /* Ask the user for the transaction date */
+
+	} while (Datestate); /* Repeat while the date is valid */
+
+
+	getCardExpiryDate(&c1);
+	
+
+	
+
+	cardexpirestatus = isCardExpired(&c1, &t1);
+	
+	
+	/*
+	maxtransamountstate = setMaxAmount(&t1, 5000.0);
+
+	printf("%d\n", maxtransamountstate);
+
+	/*
+	do
+	{
+		cardstatus = getCardHolderName(&c1);
+
+	} while (cardstatus);
+	
+
+	printf("%d", cardstatus);
+
+
 	/*
 	do
 	{
@@ -28,17 +64,18 @@ int main(void)
 	} while(Datestate); /* Repeat while the date is valid 
 	
 	*/
+	/*
 	do
 	{
-		Amountstate = getTransactionAmount(&t1) ;  /* Ask the user for the transaction amount */
+		Amountstate = getTransactionAmount(&t1) ;  /* Ask the user for the transaction amount 
 
-		Maxamountstate = isBelowMaxAmount(&t1) ;  /* Ceck the amount valid or not */
+		Maxamountstate = isBelowMaxAmount(&t1) ;  /* Ceck the amount valid or not 
 
 	} while (Maxamountstate);
 	
 
 	printf("%f", t1.transAmount);
-
+	*/
 	for (;;)
 	{
 
