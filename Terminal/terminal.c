3c9edd2996b0bf174943f1c8d1014b3c1476d001
Mustafa Mohammed <mustafamohammed0321@gmail.com>
2023-03-31 16:59:58 +0200

    first commit

---
+++ Terminal/terminal.c
@@ -0,0 +1,121 @@
+/****************************************************************************************/
+/* Author   : Mustafa Mohammed Abdou                                                    */
+/* Date     : 29 Mar , 2023                                                             */
+/* Version  : V01                                                                       */
+/* Hint     : Header file of terminal module                                            */
+/****************************************************************************************/
+#define _CRT_SECURE_NO_WARNINGS
+#define _CRT_NONSTDC_NO_WARNINGS
+
+
+#include"Terminal.h"
+
+EN_terminalError_t getTransactionDate(ST_terminalData_t* termData)
+{
+	/* Define variable from EN_terminalError_t to hold the return status */
+	EN_terminalError_t Localreturnstatus = TERMINAL_OK;
+
+	uint8_t day1 = 0 , day2 = 0 , day = 0 , month1 = 0 , month2 = 0 , month = 0 ; /* Variables for day and month */
+	 
+	uint16_t year1 = 0 , year2 = 0 , year3 = 0 , year4 = 0 , year ; /* Variable for year */
+
+	uint8_t validDate = 1 ; /* check */
+
+	printf("Enter the transaction date as DD/MM/YYYY\n"); /* Ask the user to enter the date of transaction */
+
+	scanf("%s", termData->transactionDate); /* Scan the date from user the */
+
+    /*=======================================================================*/
+	/*=                    day representation                               =*/
+	/*=======================================================================*/
+	day1 = termData->transactionDate[0] - 48; /* Convert character to number */
+	day2 = termData->transactionDate[1] - 48; /* Convert character to number */
+
+
+	day = ((day1 * 10) + day2);        /* represent the day as number */
+
+
+	/*=======================================================================*/
+	/*=                 month representation                                =*/
+	/*=======================================================================*/
+	month1 = termData->transactionDate[3] - 48; /* Convert character to number */
+	month2 = termData->transactionDate[4] - 48; /* Convert character to number */
+
+	month = ((month1 * 10) + month2); /* represent the day as number */
+
+
+	/*=======================================================================*/
+	/*=                   year representation                               =*/
+	/*=======================================================================*/
+	year1 = termData->transactionDate[6] - 48;  /* Convert character to number */
+	year2 = termData->transactionDate[7] - 48;  /* Convert character to number */
+	year3 = termData->transactionDate[8] - 48;  /* Convert character to number */
+	year4 = termData->transactionDate[9] - 48;  /* Convert character to number */
+	
+	year = ((year1 * 1000) + (year2 * 100) + (year3 * 10) + (year4) );
+	
+	/*========================================================================*/
+    /*=       validate the date in terms day, month and year                = */
+	/*========================================================================*/
+	
+	if ((day >= 1) && (day <= 31))
+	{
+		/* Do nothing */
+	}
+	else
+	{
+		validDate = 0;
+	}
+
+	if ((month >= 1) && (month <= 12))
+	{
+		/* Do nothing */
+	}
+	else
+	{
+		validDate = 0;
+	}
+
+	if (year == 2024)
+	{
+		/* Do nothing */
+	}
+	else
+	{
+		validDate = 0;
+	}
+
+	if (validDate == 0)
+	{
+		Localreturnstatus = WRONG_DATE;  /* Date is not valid */
+	}
+	else
+	{
+		Localreturnstatus = TERMINAL_OK; /* Date is valid */
+	}
+
+	return Localreturnstatus; /* Return the final status */
+}
+
+
+EN_terminalError_t getTransactionAmount(ST_terminalData_t* termData)
+{
+	EN_terminalError_t Localamountstatus = TERMINAL_OK; /* Variable to holds the function status */
+	float amount = 0;
+
+	printf("Enter the transaction amount\n"); /* Ask user to enter the amount */
+
+	scanf("%f", &amount); /* Scan amount from the user */
+	
+	if (amount <= 0) /* Invalid amount */
+	{
+		Localamountstatus = INVALID_AMOUNT ;
+	}
+	else
+	{
+		termData->transAmount = amount; /* Save the amount */
+		Localamountstatus = TERMINAL_OK ;
+	}
+
+	return Localamountstatus; /* Return the final status */
+}
---
+++ Terminal/terminal.h
@@ -0,0 +1,77 @@
+/****************************************************************************************/
+/* Author   : Mustafa Mohammed Abdou                                                    */
+/* Date     : 29 Mar , 2023                                                             */
+/* Version  : V01                                                                       */
+/* Hint     : Header file of terminal module                                            */
+/****************************************************************************************/
+
+/* HEADER FILE GUARD */
+#ifndef TERMINAL_H_
+#define TERMINAL_H_
+
+#include<stdio.h>
+
+#include"standard_types.h"
+
+
+/****************************************/
+/* typedef for terminal data includes   */
+/* 1 - trans amount                     */
+/* 2 - max trans amount                 */
+/* 3 - date of transaction              */
+/****************************************/
+typedef struct ST_terminalData_t
+{
+    float transAmount;
+    float maxTransAmount;
+    uint8_t transactionDate[11];
+}ST_terminalData_t;
+
+
+/**********************************************/
+/* enum for various return errors             */
+/* returns 0 ==> ok                           */
+/*         1 ==> wrong date                   */
+/*         2 ==> expired date                 */
+/*         3 ==> invalid card                 */
+/*         4 ==> invalid amount               */
+/*         5 ==> exceed max amount            */
+/*         6 ==> invalid max amount           */
+/**********************************************/
+typedef enum EN_terminalError_t
+{
+    TERMINAL_OK, WRONG_DATE, EXPIRED_CARD, INVALID_CARD, INVALID_AMOUNT, EXCEED_MAX_AMOUNT, INVALID_MAX_AMOUNT
+}EN_terminalError_t;
+
+
+
+/*=================================================================================================*/
+/*= Describtion   : Ask the user for transaction date and store it in terminal data               =*/
+/*= Date describtion  : 10 characters DD/MM/YYYY                                                  =*/
+/*= Inputs     : struct from ST_terminalData_t                                                    =*/
+/*= Return     : if the date isn't valid returns WRONG_DATE else returns TERMINAL_OK              =*/
+/*=================================================================================================*/
+EN_terminalError_t getTransactionDate(ST_terminalData_t* termData);
+
+
+//EN_terminalError_t isCardExpired(ST_cardData_t* cardData, ST_terminalData_t* termData);
+
+/*=====================================================================================================*/
+/*= Describtion  : Ask the user for the transaction amount to validate it eith the amount in the card =*/
+/*= Input : struct from ST_terminalData_t to save amount in it                                        =*/
+/*= Return  : if amount >= 0 return INVALID_AMOUNT else return TERMINAL_OK                            =*/
+/*=====================================================================================================*/
+EN_terminalError_t getTransactionAmount(ST_terminalData_t* termData);
+
+
+EN_terminalError_t isBelowMaxAmount(ST_terminalData_t* termData);
+
+EN_terminalError_t setMaxAmount(ST_terminalData_t* termData, float maxAmount);
+
+/* EN_terminalError_t isValidCardPAN(ST_cardData_t* cardData);  Optional */
+
+
+
+
+
+#endif 
